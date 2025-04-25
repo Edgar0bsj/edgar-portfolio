816d7717bd8a69c13ffa880a7fdc47914cadf559
Edgar Junior <edgarbsj.dev@gmail.com>
2025-04-25 13:49:11 -0300

    Update route.tsx

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -5,103 +5,30 @@ import { person } from "@/app/resources/content";
 export const runtime = "edge";
 
 export async function GET(request: Request) {
-  let url = new URL(request.url);
-  let title = url.searchParams.get("title") || "Portfolio";
-  const font = fetch(new URL("../../../public/fonts/Inter.ttf", import.meta.url)).then((res) =>
-    res.arrayBuffer(),
-  );
-  const fontData = await font;
+  const url = new URL(request.url);
+  const title = url.searchParams.get("title") || "Portfolio";
 
   return new ImageResponse(
     <div
       style={{
         display: "flex",
+        background: "#151515",
+        color: "white",
         width: "100%",
         height: "100%",
-        padding: "8rem",
-        background: "#151515",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 60,
+        fontFamily: "sans-serif",
+        padding: "2rem",
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          gap: "4rem",
-          fontFamily: "Inter",
-          fontStyle: "normal",
-          color: "white",
-        }}
-      >
-        <span
-          style={{
-            fontSize: "8rem",
-            lineHeight: "8rem",
-            letterSpacing: "-0.05em",
-            whiteSpace: "pre-wrap",
-            textWrap: "balance",
-          }}
-        >
-          {title}
-        </span>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "5rem",
-          }}
-        >
-          <img
-            src={baseURL + person.avatar}
-            style={{
-              width: "12rem",
-              height: "12rem",
-              objectFit: "cover",
-              borderRadius: "100%",
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "0.75rem",
-            }}
-          >
-            <span
-              style={{
-                fontSize: "4.5rem",
-                lineHeight: "4.5rem",
-                whiteSpace: "pre-wrap",
-                textWrap: "balance",
-              }}
-            >
-              {person.name}
-            </span>
-            <span
-              style={{
-                fontSize: "2.5rem",
-                lineHeight: "2.5rem",
-                whiteSpace: "pre-wrap",
-                textWrap: "balance",
-                opacity: "0.6",
-              }}
-            >
-              {person.role}
-            </span>
-          </div>
-        </div>
-      </div>
+      {title}
     </div>,
     {
-      width: 1920,
-      height: 1080,
-      fonts: [
-        {
-          name: "Inter",
-          data: fontData,
-          style: "normal",
-        },
-      ],
-    },
+      width: 1200,
+      height: 630,
+    }
   );
 }
+
